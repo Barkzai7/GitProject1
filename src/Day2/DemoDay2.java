4af0217ba9768f823b2f8eb34c900f9a15f17492
edris <edris@Edris-PC.myfiosgateway.com>
2023-08-01 13:09:12 -0400

    Added add()

--- src/Day2/DemoDay2.java
+++ src/Day2/DemoDay2.java
@@ -5,5 +5,12 @@ public class DemoDay2 {
 	public static void main(String[] args) {
 		System.out.println("test");
 	}
+	
+	//add method 
+	public static void add(int a, int b) {
+		
+		System.out.println(a + b);
+	}
+		
 
 }
