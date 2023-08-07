c741fa411a9d271e35394e7fc2671db6e168e628
Barkzai7 <140641450+Barkzai7@users.noreply.github.com>
2023-08-07 01:26:48 -0400

    Update DemoDay5.java

--- src/day5/DemoDay5.java
+++ src/day5/DemoDay5.java
@@ -5,8 +5,10 @@ public class DemoDay5 {
 	public static void main(String[] args) {
 		System.out.println("Today weather is rainy");
 		
-		
-		
+	
 	}
+		public void getData() {
+		System.out.println("Data....");
 
+	
 }
