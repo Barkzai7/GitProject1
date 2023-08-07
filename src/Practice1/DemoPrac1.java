f3e387e8d5b2b20c1bef9bfed09226fa595501d0
edris <edris@Edris-PC>
2023-08-06 23:04:30 -0400

    new code

--- src/Practice1/DemoPrac1.java
+++ src/Practice1/DemoPrac1.java
@@ -4,9 +4,11 @@ public class DemoPrac1 {
 	
 	public static void main(String[] args) {
 		
-		add(19, 61, "Fakhria"., "Barkzai");
+		add(19, 61, "Fakhria", "Barkzai");
 
+		System.out.println("****************");
 		
+	    classInfo("Edris", "Barkzai", "NoorTeck", 2001, 2023);
 	
 }
 		
@@ -20,6 +22,17 @@ public class DemoPrac1 {
 		
 		System.out.println(a + b);
 		System.out.println("LIFE IS EASY WITHOUT MATH");	
+		
+		
+	}
+	
+	 static void classInfo(String name, String lastName, String className, int dateOfBirth, int year) {
+		
+		System.out.println("Name: " + name);
+		System.out.println("Last Name: " + lastName);
+		System.out.println("Class Name: " + className);
+		System.out.println("DOB: " + dateOfBirth);
+		System.out.println("Year: " + year);
 		
 		
 	}
