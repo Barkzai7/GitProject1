2ecea1b4747581959b6e2c201ae10fc4cb7eb24c
edris <edris@Edris-PC>
2023-08-06 21:22:48 -0400

    new method

--- src/Practice1/DemoPrac1.java
+++ src/Practice1/DemoPrac1.java
@@ -2,13 +2,26 @@ package Practice1;
 
 public class DemoPrac1 {
 	
+	public static void main(String[] args) {
+		
+		add(3, 4);
+		
+		
+	}
+		
+	
+	
 	static void add(int a, int b) {
 		
-		a = 3;
-		b = 4;
+		
+		
+		
 		System.out.println(a + b);
-		sysout.out.println(a + b + c);		
+		System.out.println("LIFE IS EASY WITHOUT MATH");	
+		
+		
 	}
 	
 
 }
+
