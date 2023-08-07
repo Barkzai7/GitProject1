a61309904a290dcd99375cda34cbcb62dbfed220
Barkzai7 <140641450+Barkzai7@users.noreply.github.com>
2023-08-06 21:27:35 -0400

    Update DemoPrac1.java

--- src/Practice1/DemoPrac1.java
+++ src/Practice1/DemoPrac1.java
@@ -4,17 +4,19 @@ public class DemoPrac1 {
 	
 	public static void main(String[] args) {
 		
-		add(3, 4);
+		add(3, 4, Edris, Barkzai,);
+
 		
-		
-	}
+	
+}
 		
 	
 	
-	static void add(int a, int b) {
+	static void add(int a, int b, String name, String lastName) {
 		
 		
-		
+		System.out.println("Name :" + name); 
+		System.out.println("Last Name :" + lastName); 
 		
 		System.out.println(a + b);
 		System.out.println("LIFE IS EASY WITHOUT MATH");	
