059fe35307b8c6df2669560788d370577b7ba5d4
Barkzai7 <140641450+Barkzai7@users.noreply.github.com>
2023-08-06 21:15:51 -0400

    Update DemoDay3.java

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -4,6 +4,10 @@ public class DemoDay3 {
 	
 	public static void main(String[] args) {
 		System.out.println("Mohammad Edris Barkzai");
+
+		String country = "USA";
+		String state = "VA";
+		
 		
 	}
 
